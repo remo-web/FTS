84cf5e72b04f209b4148c74c1bfc848d9be6a515
Raphael <raphaelrpais@gmail.com>
2019-03-22 21:49:57 -0300

    /novo/

--- novo/_assets/js/head.js
+++ novo/_assets/js/head.js
@@ -1,14 +1,14 @@
 function ftsHead() {
     if ( document.location.href.indexOf('produtos') > -1 ) {
-        $("#fts-css-variavel").attr("href", "../_assets/css/fts-catalogo.css");
+        $("#fts-css-variavel").attr("href", "/novo/_assets/css/fts-catalogo.css");
         $("title").html("FTS | Conheça Nossos Produtos");
     }
     else if ( document.location.href.indexOf('segmentos') > -1 ) {
-        $("#fts-css-variavel").attr("href", "../_assets/css/fts-segmentos.css");
+        $("#fts-css-variavel").attr("href", "/novo/_assets/css/fts-segmentos.css");
         $("title").html("FTS | Distribuidoras, Indústrias e Aviação");
     }
     else {
-        $("#fts-css-variavel").attr("href", "/_assets/css/fts-home.css");
+        $("#fts-css-variavel").attr("href", "/novo/_assets/css/fts-home.css");
         $("title").html("FTS | Solução na Hora Que Você Precisa");
     }
 };
