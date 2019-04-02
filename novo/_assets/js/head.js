56e1778c8c77ae74c84e1df8e03e349e93e2a5ef
Raphael <raphaelrpais@gmail.com>
2019-04-02 16:17:37 -0300

    visao esrtategica
    hahaha

--- novo/_assets/js/head.js
+++ novo/_assets/js/head.js
@@ -7,6 +7,10 @@ function ftsHead() {
         $("#fts-css-variavel").attr("href", "/novo/_assets/css/fts-segmentos.css");
         $("title").html("FTS | Distribuidoras, Indústrias e Aviação");
     }
+    else if ( document.location.href.indexOf('visao') > -1 ) {
+        $("#fts-css-variavel").attr("href", "/novo/_assets/css/fts-visao.css");
+        $("title").html("FTS | Visão Estratégica");
+    }
     else {
         $("#fts-css-variavel").attr("href", "/novo/_assets/css/fts-home.css");
         $("title").html("FTS | Solução na Hora Que Você Precisa");
