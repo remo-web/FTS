4f09c3eb9620da554b2d0844b4d60485c373c211
Raphael <raphaelrpais@gmail.com>
2020-01-06 17:52:33 -0300

    problemas

--- novo/_assets/js/scroll.js
+++ novo/_assets/js/scroll.js
@@ -1,24 +1,37 @@
-$(document).ready(function(){
-  // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
+//$(document).ready(function(){
+//  // Add smooth scrolling to all links
+//  $("a").on('click', function(event) {
+//
+//    // Make sure this.hash has a value before overriding default behavior
+//    if (this.hash !== "") {
+//      // Prevent default anchor click behavior
+//      event.preventDefault();
+//
+//      // Store hash
+//      var hash = this.hash;
+//
+//      // Using jQuery's animate() method to add smooth page scroll
+//      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+//      $('html, body').animate({
+//        scrollTop: $(hash).offset().top
+//      }, 800, function(){
+//
+//        // Add hash (#) to URL when done scrolling (default click behavior)
+//        window.location.hash = hash;
+//      });
+//    } // End if
+//  });
+//});
 
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
+window.onload = function() {
+    
+    // Smooth scrolling on page load if URL have a hash
+    if(window.location.hash) {
+        var hash = window.location.hash;
 
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
-});
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 2000, 'swing');
+    };
+    
+};
