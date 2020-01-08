f6745376a67b719e93e122d42a5c672affd45651
Raphael <raphaelrpais@gmail.com>
2020-01-08 19:36:55 -0300

    removendo o novo dos links

--- assets/js/head.js
+++ assets/js/head.js
@@ -1,18 +1,18 @@
 function ftsHead() {
     if ( document.location.href.indexOf('produtos') > -1 ) {
-        $("#fts-css-variavel").attr("href", "/novo/assets/css/fts-catalogo.css");
+        $("#fts-css-variavel").attr("href", "/assets/css/fts-catalogo.css");
         $("title").html("FTS | Conheça Nossos Produtos");
     }
     else if ( document.location.href.indexOf('segmentos') > -1 ) {
-        $("#fts-css-variavel").attr("href", "/novo/assets/css/fts-segmentos.css");
+        $("#fts-css-variavel").attr("href", "/assets/css/fts-segmentos.css");
         $("title").html("FTS | Distribuidoras, Indústrias e Aviação");
     }
     else if ( document.location.href.indexOf('visao') > -1 ) {
-        $("#fts-css-variavel").attr("href", "/novo/assets/css/fts-visao.css");
+        $("#fts-css-variavel").attr("href", "/assets/css/fts-visao.css");
         $("title").html("FTS | Visão Estratégica");
     }
     else {
-        $("#fts-css-variavel").attr("href", "/novo/assets/css/fts-home.css");
+        $("#fts-css-variavel").attr("href", "/assets/css/fts-home.css");
         $("title").html("FTS | Solução na Hora Que Você Precisa");
     }
 };
